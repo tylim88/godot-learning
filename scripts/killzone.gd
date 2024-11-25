d706bbd139f61ebda67a2a4e8b411a3a86610a7b
Acid Coder <limteikyee@gmail.com>
2024-11-25 17:26:36 +0800

    done

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,8 +3,11 @@ extends Area2D
 @onready var timer: Timer = $Timer
 
 func _on_body_entered(body: Node2D) -> void:
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start() # Replace with function body.
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
 	get_tree().reload_current_scene() # Replace with function body.
